5c5c3f631c659ec0338eba565df04dc4a4f72826
Teslime <teslime_mlpnr@outlook.com>
2022-03-15 21:14:35 +0300

    A basic level bookstore project was created.

---
+++ BookStore/WebApi/Controllers/BookController.cs
@@ -0,0 +1,93 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class BookController : ControllerBase
+{ 
+
+    private static List<Book> BookList = new List<Book>(){
+                new Book(){
+                    Id = 1,
+                    Title = "Lean Startup",
+                    GenreId = 1, // Personal Growth
+                    PageCount=200,
+                    PublishDate = new DateTime(2001,06,12)
+                },
+                new Book(){
+                    Id = 2,
+                    Title = "Herland",
+                    GenreId = 2, // Science Fiction
+                    PageCount=250,
+                    PublishDate = new DateTime(2002,06,12)
+                },
+                new Book(){
+                    Id = 3,
+                    Title = "Dune",
+                    GenreId = 2, // Science Fiction
+                    PageCount=540,
+                    PublishDate =new DateTime(2002,05,23)
+                },
+          };
+
+        [HttpGet]
+        public List<Book> GetBooks()
+        {
+            var bookList = BookList.OrderBy(x => x.Id).ToList<Book>();
+            return bookList;
+        } 
+
+        [HttpGet("{id}")]
+        public Book? GetById(int id)
+        {
+            var book = BookList.Where(book => book.Id == id).SingleOrDefault();
+            return book;
+        }
+        
+        [HttpPost]
+        public IActionResult AddBook([FromBody] Book newBook)
+        {
+            var book = BookList.SingleOrDefault(x => x.Title == newBook.Title);
+            if(book is not null)
+               return BadRequest();
+            
+            BookList.Add(newBook);
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateBook(int id, [FromBody] Book updateBook)
+        {
+            var book = BookList.SingleOrDefault(x => x.Id == id);
+
+            if(book is null)
+               return BadRequest();
+            book.GenreId = updateBook.GenreId != default ? updateBook.GenreId : book.GenreId;
+            book.PageCount = updateBook.PageCount != default ? updateBook.PageCount : book.PageCount;
+            book.PublishDate = updateBook.PublishDate != default ? updateBook.PublishDate : book.PublishDate;
+            book.Title = updateBook.Title != default ? updateBook.Title : book.Title;
+
+            return Ok();
+        }
+
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteBook(int id)
+        {
+            var book = BookList.SingleOrDefault(x => x.Id == id);
+
+            if(book is null)
+                 return BadRequest();
+            
+            BookList.Remove(book);
+            return Ok();
+        }
+     /*   [HttpGet]
+        public Book? Get([FromQuery] string id)
+        {
+            var book = BookList.Where(book => book.Id == Convert.ToInt32(id)).SingleOrDefault();
+            return book;
+        }iki tane parametresiz Get metotu çalışmayacağı için yorum satırında */
+    
+}
